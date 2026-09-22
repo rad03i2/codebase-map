e09f8c25979137a18a3af34010ea86700cf9a64e
Radwan Abdulhadi Ahmed <127021904+rad03i2@users.noreply.github.com>
2026-09-22 04:04:14 +0300

    Expose public Python API

---
+++ src/codebase_map/__init__.py
@@ -0,0 +1,5 @@
+"""Codebase Map public API."""
+from .core import FileInfo, MapResult, render_tree, scan, summary
+
+__all__ = ["FileInfo", "MapResult", "scan", "render_tree", "summary"]
+__version__ = "1.0.0"
